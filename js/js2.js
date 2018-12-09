8b72a212a5b37440a74b1aa955a3066c905d2157
ILdnyy <974710986@qq.com>
2018-12-09 21:21:20 +0800

    new

--- js/js2.js
+++
@@ -1,9 +0,0 @@
-function Person(name,age){
-  this.name=name;
-  this.age=age;
-  this.introduce=function(){
-    console.log("I am " +this.name +","+ "I am " +this.age+" years old")
-  }
-}
-var jerry =new Person("Jerry", 2);
-    jerry.introduce();
