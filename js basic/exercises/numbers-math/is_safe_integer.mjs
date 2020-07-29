5bf6ff70d49f40ca39f6bed3922124368ac7a95e
ildnyy <2114829840@qq.com>
2020-07-29 20:59:55 +0800

    finish number-math

---
+++ js basic/exercises/numbers-math/is_safe_integer.mjs
@@ -0,0 +1,3 @@
+export function isSafeInteger(num){
+    return (typeof num=='number' && Math.trunc(num)==num && Number.MIN_SAFE_INTEGER <= num && Number.MAX_SAFE_INTEGER>=num)
+}
---
+++ js basic/exercises/numbers-math/parse_number.mjs
@@ -0,0 +1,3 @@
+export function parseNumber(num){
+    return Number(str);
+}
