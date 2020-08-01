7d1d84bdb091042a9a52e9ae57fa2cb03bdf2562
ildnyy <2114829840@qq.com>
2020-08-01 22:02:42 +0800

    finish sync

--- js basic/exercises/sync-iteration-use/sync_iteration_manually_exrc.mjs
+++ js basic/exercises/sync-iteration-use/sync_iteration_manually_exrc.mjs
@@ -1,18 +1,12 @@
-/* npm t exercises/sync-iteration-use/sync_iteration_manually_exrc.mjs
-Instructions:
-– Use an iterator for arr instead of the current indexed access.
-– Use assert.deepEqual() to check the objects returned by the iterator.
-– That is: the assertions also check content and length of the array, but look quite different.
-*/
-
 import test from 'ava';
 import {strict as assert} from 'assert';
 
 test('instanceof', t => {
   const arr = ['a', 'b', 'c'];
 
-  assert.equal(arr[0], 'a');
-  assert.equal(arr[1], 'b');
-  assert.equal(arr[2], 'c');
-  assert.equal(arr.length, 3);
+  const iter = arr[Symbol.iterator]();
+  assert.deepEqual(iter.next(), {value: 'a', done: false });
+  assert.deepEqual(iter.next(), {value: 'b', done: false });
+  assert.deepEqual(iter.next(), {value: 'c', done: false });
+  assert.deepEqual(iter.next(), {value: undefined, done: true });
 });
