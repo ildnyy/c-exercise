82c38744e7c3a43c290f709d8c504052c5cc147b
ildnyy <2114829840@qq.com>
2020-08-03 22:20:53 +0800

    finish: variables-assignment

--- js basic/exercises/variables-assignment/const_exrc.mjs
+++ js basic/exercises/variables-assignment/const_exrc.mjs
@@ -1,9 +1,3 @@
-/* npm t exercises/variables-assignment/const_exrc.mjs
-Instructions:
-- Run this test (it fails).
-- Insert a single(!) const statement so that the test passes.
-*/
-
 import test from 'ava';
 import {strict as assert} from 'assert';
 
@@ -11,6 +5,7 @@ test('const', t => {
     const x = 3;
     assert.equal(x, 3);
     {
+        const x = 12;
         assert.equal(x, 12);
     }
     assert.equal(x, 3);
